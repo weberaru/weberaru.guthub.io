c00215b5ef56a93dce2659f3630c5c1b24cb3a7e
liccilip <5@3243.ru>
2019-01-30 04:01:02 +0300

    фор1

--- formdata.php
+++ formdata.php
@@ -1,5 +1,5 @@
 <?php
-$resuit = mail("ИМЯ $_POST[ez]");
+$resuit = mail("ИМЯ: $_POST[ez0] Сообщение: $_POST[ez1] Тема: $_POST[ez2] Сам текст: $_POST[ez3]");
 
 if ($result) {
     echo "<p>Сообщение отправлино успешно!</p>";
